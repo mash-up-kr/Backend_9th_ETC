473dd3740657a2ec7f1a799962f0d4a69fd81925
ihaeeun <ihaeeun16@gmail.com>
2020-05-18 12:25:07 +0900

    Create project

---
+++ src/main/java/mashup/backend/etc/our/OurApplication.java
@@ -0,0 +1,13 @@
+package mashup.backend.etc.our;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mashup/backend/etc/our/OurApplicationTests.java
@@ -0,0 +1,13 @@
+package mashup.backend.etc.our;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
