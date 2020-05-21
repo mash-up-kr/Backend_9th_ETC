df4cf7240bfee1c3cf6b1e0664b17f4f79f4ddb3
YJeongKim <yjeong20s@naver.com>
2020-05-22 05:51:51 +0900

    [#9] Feat : Group 리스트 조회 기능 추가
    - JPA Auditing을 위한 BaseTimeEntity 추가
    
    - Group 리스트 조회 관련 코드 추가

--- src/main/java/mashup/backend/etc/our/group/entity/Group.java
+++ src/main/java/mashup/backend/etc/our/group/entity/Group.java
@@ -1,4 +1,28 @@
 package mashup.backend.etc.our.group.entity;
 
-public class Group {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mashup.backend.etc.our.common.domain.BaseTimeEntity;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Group extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupId;
+
+    @Column(length = 45, nullable = false)
+    private String name;
+
+    @Column(length = 45, nullable = false)
+    private String code;
+
+    @Builder
+    public Group(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/mashup/backend/etc/our/group/repository/GroupRepository.java
+++ src/main/java/mashup/backend/etc/our/group/repository/GroupRepository.java
@@ -1,4 +1,12 @@
 package mashup.backend.etc.our.group.repository;
 
-public interface GroupRepository {
+import mashup.backend.etc.our.group.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    @Query("SELECT g.groupId, g.name FROM Group g ORDER BY g.groupId DESC")
+    List<Group> readGroupList();
 }
