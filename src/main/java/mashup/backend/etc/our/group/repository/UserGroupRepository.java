ebb46fab07fe28e6beb05e513e8a750df045fd8e
연정 <33328991+YJeongKim@users.noreply.github.com>
2020-05-23 17:03:06 +0900

    [#9] Fix : Group 리스트 조회 API 수정 (#15)
    * [#9] Feat : Group 리스트 조회 기능 추가
    
    - JPA Auditing을 위한 BaseTimeEntity 추가
    
    - Group 리스트 조회 관련 코드 추가
    
    * [#9] Feat : Group 리스트 조회 기능 추가
    
    - UserGroup Entity, Repository 추가
    
    - 스프링 버전 변경 (2.3.0 -> 2.1.7)
    
    - Group 리스트 조회 기능 추가
    
    * [#9] Fix : Group 리스트 조회 API 수정
    
    - 엔드포인트 수정
    
    - repository의 메소드와 쿼리 수정
    
    - Controller에 응답 객체를 ResponseEntity로 변경

--- src/main/java/mashup/backend/etc/our/group/repository/UserGroupRepository.java
+++ src/main/java/mashup/backend/etc/our/group/repository/UserGroupRepository.java
@@ -1,11 +1,12 @@
 package mashup.backend.etc.our.group.repository;
 
-import mashup.backend.etc.our.group.entity.Group;
 import mashup.backend.etc.our.group.entity.UserGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserGroupRepository  extends JpaRepository<UserGroup, Long> {
+    List<UserGroup> findAllByUserId(Long userId);
 }
