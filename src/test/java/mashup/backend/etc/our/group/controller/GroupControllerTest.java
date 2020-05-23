61b12f2a733de968dc2820766ab40816d7828cc8
YJeongKim <yjeong20s@naver.com>
2020-05-23 20:22:54 +0900

    [#10] Fix : Conflict 코드 수정
    - GroupConrollerTest, GroupRepositoryTest 수정

--- src/test/java/mashup/backend/etc/our/group/controller/GroupControllerTest.java
+++ src/test/java/mashup/backend/etc/our/group/controller/GroupControllerTest.java
@@ -1,22 +1,6 @@
 package mashup.backend.etc.our.group.controller;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4ca5ea6... [#9] Feat : Group 리스트 조회 기능 추가
-=======
->>>>>>> a8f6066... Group 리스트 조회 API (#14)
-import mashup.backend.etc.our.group.dto.ReqGroupListDto;
-import mashup.backend.etc.our.group.dto.ResGroupListDto;
 import mashup.backend.etc.our.group.entity.Group;
-=======
->>>>>>> 139e4af... [#10] Group 생성 API 추가
-=======
->>>>>>> e3d23f6597d1e76653947e6a256fd58a7c16e4a5
 import mashup.backend.etc.our.group.repository.GroupRepository;
 import org.junit.After;
 import org.junit.runner.RunWith;
@@ -30,8 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-<<<<<<< HEAD
-<<<<<<< HEAD
 public class GroupControllerTest {
     @LocalServerPort
     private int port;
@@ -46,32 +28,4 @@ public class GroupControllerTest {
     public void cleanup() throws Exception {
         groupRepository.deleteAll();
     }
-=======
-public class GroupControllerTest {
->>>>>>> 02a07f3... [#6] Chore : 패키지 구조 설계
-=======
-public class GroupControllerTest {
->>>>>>> 01d2806... [#6] Chore : 패키지 구조 설계 (#13)
-=======
-public class GroupControllerTest {
-=======
-public class GroupControllerTest {
->>>>>>> a8f6066... Group 리스트 조회 API (#14)
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private GroupRepository groupRepository;
-
-    @After
-    public void cleanup() throws Exception {
-        groupRepository.deleteAll();
-    }
-<<<<<<< HEAD
->>>>>>> 4ca5ea6... [#9] Feat : Group 리스트 조회 기능 추가
-=======
->>>>>>> a8f6066... Group 리스트 조회 API (#14)
 }
--- src/test/java/mashup/backend/etc/our/group/repository/GroupRepositoryTest.java
+++ src/test/java/mashup/backend/etc/our/group/repository/GroupRepositoryTest.java
@@ -1,14 +1,5 @@
 package mashup.backend.etc.our.group.repository;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4ca5ea6... [#9] Feat : Group 리스트 조회 기능 추가
-=======
->>>>>>> a8f6066... Group 리스트 조회 API (#14)
-import mashup.backend.etc.our.group.entity.Group;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,14 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-<<<<<<< HEAD
-<<<<<<< HEAD
 public class GroupRepositoryTest {
     @Autowired
     GroupRepository groupRepository;
@@ -32,26 +19,4 @@ public class GroupRepositoryTest {
     public void cleanup() {
         groupRepository.deleteAll();
     }
-=======
-public class GroupRepositoryTest {
->>>>>>> 02a07f3... [#6] Chore : 패키지 구조 설계
-=======
-public class GroupRepositoryTest {
->>>>>>> 01d2806... [#6] Chore : 패키지 구조 설계 (#13)
-=======
-public class GroupRepositoryTest {
-=======
-public class GroupRepositoryTest {
->>>>>>> a8f6066... Group 리스트 조회 API (#14)
-    @Autowired
-    GroupRepository groupRepository;
-
-    @After
-    public void cleanup() {
-        groupRepository.deleteAll();
-    }
-<<<<<<< HEAD
->>>>>>> 4ca5ea6... [#9] Feat : Group 리스트 조회 기능 추가
-=======
->>>>>>> a8f6066... Group 리스트 조회 API (#14)
 }
